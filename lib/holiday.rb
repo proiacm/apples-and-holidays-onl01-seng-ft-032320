ab55c34dbe485c59c0546802dc77f2a132383284
CiaraMaria Proia <proiacm@gmail.com>
2020-03-27 21:59:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,6 +57,7 @@ def all_winter_holiday_supplies(holiday_hash)
      end
    end    
 end 
+holiday_hash
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
