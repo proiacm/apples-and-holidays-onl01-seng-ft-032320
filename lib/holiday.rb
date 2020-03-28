519155356392a6fa2c7a32a615ac7a8d79541f7b
CiaraMaria Proia <proiacm@gmail.com>
2020-03-28 01:35:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,7 +77,7 @@ def all_holidays_with_bbq(holiday_hash)
 holiday_hash.map do |season, holiday|
   holiday.map do |day, supply|
     if supply.include?"BBQ"
-      day.compact
+      day.flatten.compact
       end
     end
   end
