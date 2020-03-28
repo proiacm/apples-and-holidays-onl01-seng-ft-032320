d97eb0f6d040981c85818272345c0cf6811d11fd
CiaraMaria Proia <proiacm@gmail.com>
2020-03-28 01:33:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,12 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+holiday_hash.map do |season, holiday|
+  holiday.map do |day, supply|
+    if supply.include?"BBQ"
+      day
+    end
+  end
 end
 
 
