26b88801f2ba0ea77feea652d73c6609f6bd58eb
CiaraMaria Proia <proiacm@gmail.com>
2020-03-28 01:21:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,8 +64,8 @@ def all_supplies_in_holidays(holiday_hash)
 
 holiday_hash.each do |season, holiday|
   puts "#{season.to_s.capitalize}:"
-  holiday.each do |day, supply|
-    puts " #{day.to_s.split("_").map { |x| x.capitalize }.join(" ") }: #{supply.join(", ")}"
+  holiday.each do |event, supplies|
+    puts " #{event.to_s.split("_").map { |x| x.capitalize }.join(" ") }: #{supplies.join(", ")}"
     end
   end
 end
